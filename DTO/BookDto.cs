ce92c431c3b192207f628ac839edafb3e9c5c43d
xuxiaolei <xuchangqing@myweimai.com>
2017-03-22 13:44:02 +0800

    add

---
+++ DTO/BookDto.cs
@@ -0,0 +1,10 @@
+﻿namespace DTO
+{
+    public class BookDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string AuthorName { get; set; }
+
+    }
+}
---
+++ UsingWebAPIWithEF6/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace UsingWebAPIWithEF6.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public decimal Price { get; set; }
+        public string Genre { get; set; }
+        //Foreign Key
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+    }
+}
